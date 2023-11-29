f532ab8065fd1cba22177696d5bbf6fe0dffdd1d
Sanju2409 <sanjanashetty.2409@gmail.com>
2023-11-29 21:15:18 +0530

    Add project files.

---
+++ ExpenseTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+    public class ApplicationDbContext:DbContext //inheriting DbContext base class
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) //instance of this will be created ,that can be done using dependency injection in program.cs
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; } //tables corresponding to these two will be named as transactions and categories
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //categoryid
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } //navigational property:it is to implement foreign key relationship with base Category model
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; } //? indicates it can be nullable
+
+        public DateTime Date { get; set; }= DateTime.Now;
+    }
+}
