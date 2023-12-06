6089455858392bc621ec7376000230c274fb292e
Sanju2409 <sanjanashetty.2409@gmail.com>
2023-12-07 00:52:12 +0530

    Added transaction controller

--- ExpenseTracker/Models/Transaction.cs
+++ ExpenseTracker/Models/Transaction.cs
@@ -11,12 +11,30 @@ namespace ExpenseTracker.Models
 
         //categoryid
         public int CategoryId { get; set; }
-        public Category Category { get; set; } //navigational property:it is to implement foreign key relationship with base Category model
+        public Category? Category { get; set; } //navigational property:it is to implement foreign key relationship with base Category model
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; } //? indicates it can be nullable
 
         public DateTime Date { get; set; }= DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category==null ? " ":Category.Icon + "  " + Category.Title;  
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ( (Category == null || Category.Type=="Expense")? "- " : "+ ") +Amount.ToString("C0");
+            }
+        }
     }
 }
